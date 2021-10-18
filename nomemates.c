49a14fa7ed9e64cacef8a41200d2d47ae99ce997
jorgais1234 <a01634630@itesm.mx>
2021-10-18 23:09:28 +0000

    .

---
+++ nomemates.c
@@ -0,0 +1,21 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+
+void handleTerm(int signal){
+    printf("Recibi signal %d\n", signal); 
+}
+
+
+int main(){
+    signal(15,handleTerm);
+    while(1){
+        printf("Trabajando \n"); 
+        sleep(1); 
+
+    }
+    return 0; 
+
+}
